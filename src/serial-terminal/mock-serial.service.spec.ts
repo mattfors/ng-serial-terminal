86bec14b336b20f8d8be42af20ff162ef5c707e9
Matthew Fors <1824866+mattfors@users.noreply.github.com>
2024-11-16 12:44:08 -0500

    feature: setup github workflow

--- src/serial-terminal/mock-serial.service.spec.ts
+++ src/serial-terminal/mock-serial.service.spec.ts
@@ -1,13 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MockSerialService } from './mock-serial.service';
+import { SerialService } from './serial.service';
 
 describe('MockSerialService', () => {
   let service: MockSerialService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MockSerialService);
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: SerialService, useClass: MockSerialService },
+      ]
+    });
+    service = TestBed.inject(SerialService);
   });
 
   it('should be created', () => {
