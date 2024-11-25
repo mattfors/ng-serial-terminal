c893db45be8b43316ec03059abd1a8771f346601
Matthew Fors <1824866+mattfors@users.noreply.github.com>
2024-11-25 18:29:21 -0500

    test: fix test

--- src/serial-terminal/serial-terminal.component.spec.ts
+++ src/serial-terminal/serial-terminal.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SerialTerminalComponent } from './serial-terminal.component';
+import { provideAngularSerialTest } from 'angular-web-serial';
 
 describe('SerialTerminalComponent', () => {
   let component: SerialTerminalComponent;
@@ -8,10 +9,11 @@ describe('SerialTerminalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SerialTerminalComponent]
+      imports: [SerialTerminalComponent],
+      providers: [provideAngularSerialTest()]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(SerialTerminalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
